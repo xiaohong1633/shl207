da58246bedd1ddd7136ed10dfbcf60ffebcb173c
xiaohong <805642585@qq.com>
2016-01-05 18:19:27 +0800

    shl v1.0.11

--- table.php
+++ table.php
@@ -1,8 +1,3 @@
-<!--<php
-	require_once("./DB/dBServer.php");
-	$dbs = new DBServer();
-	$json = $dbs->squery();
-?>-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -126,7 +121,7 @@ $(function(){
 		var mjson = eval(data);
 		$("#div").table(mjson);
 	});*/
-	$.post("http://localhost:8080/shl207/mytable/data.json",{
+	$.post("/shl207/mytable/data.json",{
 		id:1
 	},function(data){
 		$("#div").table(data);
