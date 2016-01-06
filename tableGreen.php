b66b5f5bb7907f262398e180976bad15e4aadb90
xiaohong <805642585@qq.com>
2016-01-06 21:55:25 +0800

    shl v1.0.19

--- tableGreen.php
+++ tableGreen.php
@@ -21,6 +21,11 @@
 	.table-bordered > tbody > tr > td.ptd{
 		padding:0;	
 	}
+	.pager li{
+		font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+		font-size: 14px;
+		line-height: 1.42857;		
+	}
 	.pager li > span{
 		color: #337ab7;	
 	}
