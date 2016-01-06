66ccff5eca85cd5b7acf32416565626c01223a79
xiaohong <805642585@qq.com>
2016-01-06 22:02:20 +0800

    shl v1.0.21

--- tableGreen.php
+++ tableGreen.php
@@ -25,7 +25,7 @@
 		font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
 		font-size: 14px;
 		line-height: 1.42857;
-		vertical-align:baseline;		
+		vertical-align:central;		
 	}
 	.pager li > span{
 		color: #337ab7;	
